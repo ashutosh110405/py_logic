f33fbce6d9b7ebfc4e8b75f933680a420131a758
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-21 10:54:41 +0530

    e

---
+++ rev_print.py
@@ -0,0 +1,8 @@
+#print reverse input int without string
+n = int(input("Enter an integer: "))
+rev = 0
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
+    n = n // 10
+print("Reverse of the number is:", rev)

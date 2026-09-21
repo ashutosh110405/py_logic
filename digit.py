dbbc4add899900b0910e7423f613a07e53b8c905
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-21 10:47:29 +0530

    d

--- digit.py
+++ digit.py
@@ -1,4 +1,4 @@
-#take input from user and find out how much input is digits
+#take input from user and find out how much input is digits onlu count digit not char or any symbol
 n = int(input("Enter a number: "))
-num = sum(1 for i in str(n))
+num = sum(1 for i in str(n) if i.isdigit())
 print(f"Number of digits in the input: {num}")

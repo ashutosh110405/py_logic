7eb704b71d0a560764163db34a08ec3542677acb
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-21 10:38:15 +0530

    d

--- digit.py
+++ digit.py
@@ -1,4 +1,4 @@
 #take input from user and find out how much input is digits
 n = int(input("Enter a number: "))
-num = sum(1 for c in str(n))
+num = sum(1 for i in str(n))
 print(f"Number of digits in the input: {num}")
